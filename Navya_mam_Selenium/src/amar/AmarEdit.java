59e2f01fed851120e54a6abe66070123722c8e3d
kumsu <kumsu@King_Vishwa>
2024-11-18 20:21:40 +0530

    edit

---
+++ Navya_mam_Selenium/src/amar/AmarEdit.java
@@ -0,0 +1,14 @@
+package amar;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AmarEdit
+{
+@Test
+
+public void gitProj()
+{
+	Reporter.log("Send to GitHub");
+}
+}
